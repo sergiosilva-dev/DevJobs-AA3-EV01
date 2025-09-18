24271bd3b398b4ab8115fefbc5d18bc78ec21bdf
Sergio Silva <ssilva@updevelop.com.co>
2025-09-18 14:29:14 -0500

    feat(bootstrap): Spring Boot 3 (Web, Thymeleaf, JPA, Validation, H2, PG, Actuator) + CI

---
+++ src/test/java/com/devjobs/DevjobsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devjobs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevjobsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
